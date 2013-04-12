71610d043cdc0c1847c1598e50f8574018e2fd3e
Eric Akaoka <eric.akaoka@gmail.com>
2013-04-12 11:32:16 -0400

    Responsive!

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,33 @@
 //= require_tree .
+
+var gridResize = function() {
+	var viewportWidth = $(window).width();
+	var viewportHeight = $(window).height();
+	
+	var gridWidth;
+	var gridHeight;
+	var gridPadding = ($('#maingrid').outerWidth()/200) + (( $("header").height() + $("footer").height() )*2);
+	
+	if ( viewportHeight < viewportWidth ) {
+		gridHeight = viewportHeight - gridPadding;
+		gridWidth = gridHeight;
+	} else {
+		gridWidth = viewportWidth - gridPadding;
+		gridHeight = gridWidth;
+	}
+	
+	$("#maingrid").height(gridHeight).width(gridWidth).css({
+        position:'absolute',
+        left: ($(window).width() - $('#maingrid').outerWidth())/2,
+        top: ($(window).height() - $('#maingrid').outerHeight())/2
+    });
+}
+
+$(document).ready(function() {
+	document.ontouchmove = function(e) {e.preventDefault()};
+	
+	gridResize();
+	$(window).on("resize", function() {
+		gridResize();
+	});
+});
